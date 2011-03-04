d9094d6ad39429763273e38b029a2cb3a6690b6b
Rafael Ortiz <rafael@activitycentral.com>
2011-03-04 12:10:23 -0500

    Adding spanish comments, bundle_id

--- activity.py
+++ activity.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """HelloMesh Activity: A case study for collaboration using Tubes."""
-
+"""Actividad HelloMesh: Un caso de estudio para colaboracion usando Tubos."""
 import logging
 import telepathy
 
